24406354c3fb0f8ee5b714ebb1ca4de34ba59821
Mariusz Klinikowski <nikowek@gmail.com>
2015-12-24 12:13:36 +0100

    setup.sh will add PATH line for  ~/.bashrc

--- setup.sh
+++ setup.sh
@@ -14,4 +14,8 @@ for x in *;
 	ln -v -s $toolsDirectory/$x $localBinDirectory/$x
 done
 
+# Add path file to bashrc
+echo "# Add local utils to path" >> $HOME/.bashrc
+echo "export PATH=$localBinDirectory:\$PATH" >> $HOME/.bashrc
+
 exit 0
